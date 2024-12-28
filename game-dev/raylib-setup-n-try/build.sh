15629bf4a44b2e9147d3b225a293c8d34fb4ba67
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-12-28 12:46:10 +1100

    chore(raylib): remove confusing build script

--- game-dev/raylib-setup-n-try/build.sh
+++
@@ -1,5 +0,0 @@
-executable="game"
-export MACOSX_DEPLOYMENT_TARGET=10.12
-clang src/main.c -I ./include/raylib -L./lib/raylib -lraylib -o $executable -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL
-# clang main.c -I ./raylib/include -lraylib -o $executable -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL
-./$executable
